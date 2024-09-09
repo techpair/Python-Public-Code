fb4fb73ffe0b198f52d149c787e4cd8966212db7
Tristan De Luna <44929175+techpair@users.noreply.github.com>
2024-09-09 09:16:50 +0800

    Create Automate the Boring Stuff - Functions.py

---
+++ Automate the Boring Stuff - Functions.py
@@ -0,0 +1,14 @@
+# Built-In Functions/Modules
+
+import random, sys, os, math
+from random import *
+import sys
+
+random.randint(1,10)
+randint(1,10)
+sys.exit()
+
+# Third Party Modules - use pip
+# In command line, type cd "Program Files\Python 3.5\Scripts"
+# In command line, type pip.exe install moduleName
+# In command line, type exit
