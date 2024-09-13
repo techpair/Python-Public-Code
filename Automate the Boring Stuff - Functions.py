d87675921b57f6d327d911e1cc12a193c82dd9cd
Tristan De Luna <44929175+techpair@users.noreply.github.com>
2024-09-13 15:56:36 +0800

    Update Automate the Boring Stuff - Functions.py

--- Automate the Boring Stuff - Functions.py
+++ Automate the Boring Stuff - Functions.py
@@ -61,4 +61,24 @@ try:
 except ValueError:
   print("Should be numerical")
   
+import random
+print("Hello what is your name?")
+name = input()
+secretNumber = random.randint(1,20)
+print("Well, " + name + ", I am thinking of a number between 1 to 20. Can you guess the number?")
 
+for guessesTaken in range(1,7):
+  print("take a guess..")
+  guess = int(input())
+
+  if guess<secretNumber:
+    print("Your guess is too low")
+  elif guess>secretNumber:
+    print("Your guess is too high")
+  else:
+    break
+  
+if guess == secretNumber:
+  print("Good job, " + name + "! You guessed my number in " + str(guessesTaken) + " tries.")
+else:
+  print("Nope, the number I was thinking of was " + str(secretNumber))
