790cb1b6042f9be577dfc7a77f163e919c6253c9
Tristan De Luna <44929175+techpair@users.noreply.github.com>
2024-09-17 02:28:28 +0800

    Update Automate the Boring Stuff - Lists.py

--- Automate the Boring Stuff - Lists.py
+++ Automate the Boring Stuff - Lists.py
@@ -1,1 +1,24 @@
 
+spam = ['cat', 'dog']
+spam[0]
+print(spam[0])
+
+spam = [['cat','dog'],[10,20,30,40,50]]
+print(spam[1][3])
+print(spam[0][-1])
+
+slicer = ['cat', 'bat', 'rat', 'elephant']
+print(slicer[0:1]) # doesn't include the index on the right
+print(slicer[1:])
+print(slicer[:3])
+
+# del spam[1][4]
+print(len([1,2,3]))
+
+print([1,2,3] + [4,5,6])
+print("hello"*3)
+print([1,2,3]*3)
+int('42')
+str(42)
+list("Hello")
+
