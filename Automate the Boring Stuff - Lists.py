dece73d5b06b4eec692eeebbbfc7480d3331e096
Tristan De Luna <44929175+techpair@users.noreply.github.com>
2024-09-18 15:41:08 +0800

    Update Automate the Boring Stuff - Lists.py

--- Automate the Boring Stuff - Lists.py
+++ Automate the Boring Stuff - Lists.py
@@ -1,4 +1,4 @@
-
+# The list data type
 spam = ['cat', 'dog']
 spam[0]
 print(spam[0])
@@ -21,4 +21,46 @@ print([1,2,3]*3)
 int('42')
 str(42)
 list("Hello")
+
+# For loops with lists, multiple assignment, and augmented operators
+
+for i in range(4):
+    print(i)
+
+print(list(range(4)))
+print(list(range(0, 100, 2)))
+
+supplies = ['pens', 'staplers', 'bin']
+for i in range(len(supplies)):
+    print('Index' + str(i) + ' in supplies is ' + supplies[i])
+
+
+supplies = ['pens' , 'pens','pens' , 'pens','pens' , 'pens','pens' , 'pens']
+for i in range(len(supplies)):
+    print('Index' + str(i) + ' in supplies is ' + supplies[i])
+
+cat = ['fat','orange','loud']
+
+size = cat[0]
+color = cat[1]
+disposition = cat[2]
+
+
+size, color, disposition = cat
+print(size)
+print(color)
+print(disposition)
+size, color, disposition = 'skinny', 'black', 'quiet'
+print(size)
+print(color)
+print(disposition)
+a='AAA'
+b='BBB'
+a,b = b,a
+print(a)
+print(b)
+
+spam = 42
+spam = spam +1
+spam +=1
 
