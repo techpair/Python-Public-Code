cd8994c703d820a25787138aaee33b3345265397
Tristan De Luna <44929175+techpair@users.noreply.github.com>
2024-09-24 14:58:44 +0800

    Update Automate the Boring Stuff - Lists.py

--- Automate the Boring Stuff - Lists.py
+++ Automate the Boring Stuff - Lists.py
@@ -1,4 +1,3 @@
-# The list data type
 spam = ['cat', 'dog']
 spam[0]
 print(spam[0])
@@ -21,8 +20,6 @@ print([1,2,3]*3)
 int('42')
 str(42)
 list("Hello")
-
-# For loops with lists, multiple assignment, and augmented operators
 
 for i in range(4):
     print(i)
@@ -87,3 +84,63 @@ print(sortedThisAnimalNames)
 # Ascii betical order; lower cases come after upper cases
 
 sortedThisAnimalNames.sort(key=str.lower) # to sort on true alphabetical order regardless of the case
+
+# relationships between lists and strings
+
+# similarities between lists and strings: indexing, looping, assignment operators
+print(list('Hello'))
+name = 'Zophie'
+print(name[0])
+print(name[1:3])
+print(name[-2])
+print('Zo' in name)
+
+for letter in name:
+    print(letter)
+
+# immutable and mutable data types
+name = 'ZOphie the cat'
+print(name[7])
+try:
+    name[7] = 'X' # this will result in an errow since strings are immutable
+except:
+    print('Error while changing immutable data')
+    
+# creating new strings from slices
+name = 'Zophie a cat'
+newName = name[0:7] + 'the ' + name[8:12]
+print(newName)
+
+# difference between immutable and mutable comes up with "references"
+# lists uses references
+
+spam = [0, 1, 2, 3, 4, 5]
+cheese = spam
+cheese[1] = 'Hello'
+print(cheese)
+print(spam) # spam also changed since it is equal to cheese by reference since these are mutable values
+
+def eggs(someParameter):
+    someParameter.append('Hello')
+
+spam = [1, 2, 3]    
+eggs(spam) # lists are mutable
+print(spam)
+
+import copy
+
+spam = ['A', 'B', 'C', 'D']
+cheese = copy.deepcopy(spam)
+cheese[1] = 42
+print(cheese)
+print(spam)
+
+# line continuation
+spam = ['apples',
+        'oranges',
+        'bananas',
+        'cats']
+
+print(spam)
+print('four score and severn' + \
+      'years ago')
